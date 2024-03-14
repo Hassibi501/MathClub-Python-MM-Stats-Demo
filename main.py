96b2fe8af00228a3dc69cc63647a0738a01af67d
Hassieb <hassieb.n.h@gmail.com>
2024-03-14 08:38:25 -0700

    Made a loop for the Menu.

--- main.py
+++ main.py
@@ -6,34 +6,40 @@ import matplotlib.colors as mcolors
 import pandas as pd
 import data
 
-print("\n")
-print("================================================================")
-print("                        Graph & Data Options")
-print("1. Main Data")
-print("2. Bar Graph (Greatest to Least)")
-print("3. Pie Chart")
-print("4. Exit")
-print("================================================================")
-print("\n")
+while True:
+    print("\n")
+    print("================================================================")
+    print("                        Graph & Data Options")
+    print("1. Main Data")
+    print("2. Bar Graph (Greatest to Least)")
+    print("3. Pie Chart")
+    print("4. Exit")
+    print("================================================================")
+    print("\n")
 
-MMs = input("How would you like to display your data")
+    MMs = input("How would you like to display your data? ")
 
-match MMs:
-    case '1':
-         df = data.mainData(show_table=True)
-         print(df)
-         exit()
-        
-    case '2':
-        sorted_df = data.sortedTable(showBarGraph=True)
-        print(sorted_df )
-    case '3':
-        data.pieChart(showPieChart = True)
-        print("Pie Chart has been displayed successfully.")
-    case '4':
-        exit()
-    case _:
+    print("\n")
+
+    match MMs:
+        case '1':
+            df = data.mainData(show_table=True)
+            print(df)
+            continue
+            
+        case '2':
+            sorted_df = data.sortedTable(showBarGraph=True)
+            print(sorted_df )
+            continue
+        case '3':
+            data.pieChart(showPieChart = True)
+            print("Pie Chart has been displayed successfully.")
+            continue
+        case '4':
+            break  
+        case _:
             print("Error test")
+            continue
 
 
 
